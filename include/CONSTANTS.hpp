19443a4bbb8ebaecff2bb6b2f800796c982e7be8
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-07-05 16:02:58 -0300

    feat: adding constants file with operations TAGS

---
+++ include/CONSTANTS.hpp
@@ -0,0 +1,11 @@
+#ifndef INSERT_MAIL_TAG
+#define INSERT_MAIL_TAG "ENTREGA"
+#endif
+
+#ifndef SEARCH_MAIL_TAG
+#define SEARCH_MAIL_TAG "CONSULTA"
+#endif
+
+#ifndef DELETE_MAIL_TAG
+#define DELETE_MAIL_TAG "APAGA"
+#endif
