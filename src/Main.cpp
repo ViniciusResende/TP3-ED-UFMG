d4eb51371a1db49fec911915c3b05136a3db8fb9
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-07-05 14:58:05 -0300

    feat: preparing main file to receive coplation inputs

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,7 +9,6 @@
 #include "msgassert.h"
 
 struct ConfigStruct {
-  int quickSortPivot, sortingAlgorithmBreakpoint;
   bool regmem;
   char inputFile[100], outputFile[100], logname[100] = "/tmp/email_server_log.out";
 };
@@ -22,9 +21,7 @@ ConfigStruct config;
 void menu() {
   fprintf(stderr,"Google Mail Server\n");
   fprintf(stderr,"-[i|I] <file>\t(Input File)\n");
-  fprintf(stderr,"-[o|O] <file>\t(output matrix file)\n");
-  fprintf(stderr,"-[m|M] <number>\t(Quick Sort Pivot Median)\n");
-  fprintf(stderr,"-[s|S] <number>\t(Breakpoint Size to Change Sort Algorithm)\n");
+  fprintf(stderr,"-[o|O] <file>\t(Output file)\n");
   fprintf(stderr,"-l \t\t(register memory access)\n");
 }
 
@@ -39,32 +36,19 @@ void parse_args(int argc,char ** argv) {
 
   int c;
 
-  config.quickSortPivot = -1;
-  config.sortingAlgorithmBreakpoint = -1;
   config.regmem = false;
   config.inputFile[0] = 0;
   config.outputFile[0] = 0;
 
   // getopt - letra indica a opcao, : junto a letra indica parametro
   // no caso de escolher mais de uma operacao, vale a ultima
-  while ((c = getopt(argc, argv, "i:I:o:O:m:M:s:S:lp:")) != EOF)
+  while ((c = getopt(argc, argv, "i:I:o:O:l")) != EOF)
     switch(tolower(c)) {
       case 'i': 
         strcpy(config.inputFile, optarg);
         break;
       case 'o': 
         strcpy(config.outputFile, optarg);
-        break;
-      case 'm':
-        errorAssert(atoi(optarg) > 0, "Invalid Quick Sort pivot median provided");
-        config.quickSortPivot = atoi(optarg);
-        break;
-      case 's':
-        errorAssert(atoi(optarg) > 0, "Invalid Breakpoint Size provided");
-        config.sortingAlgorithmBreakpoint = atoi(optarg);
-        break;
-      case 'p': 
-        strcpy(config.logname, optarg);
         break;
       case 'l': 
         config.regmem = true;
@@ -76,17 +60,9 @@ void parse_args(int argc,char ** argv) {
     }
 
   errorAssert(strlen(config.inputFile) > 0,
-    "Analyze and Sort - input file name must be previously defined");
+    "Google Mail Server - input file name must be previously defined");
   errorAssert(strlen(config.outputFile) > 0,
-    "Analyze and Sort - output file name must be previously defined");  
-  if(config.sortingAlgorithmBreakpoint == -1) {
-    warnAssert(false, "Algorithm Breakpoint Size wasn't provided keeping default of 20");
-    config.sortingAlgorithmBreakpoint = 20;
-  }
-  if(config.quickSortPivot == -1) {
-    warnAssert(false, "Quick Sort pivot median wasn't provided keeping default of 1");
-    config.quickSortPivot = 1;
-  }
+    "Google Mail Server - output file name must be previously defined");  
 }
 
 /**
