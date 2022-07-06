2b400db8c27e8c163ccd2db236100230b34aa357
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-07-06 11:58:38 -0300

    feat: implementing file reading, mail insert and mail search at Main file

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,10 +5,13 @@
 #include <fstream>
 #include <getopt.h>
 #include <string.h>
+#include "CONSTANTS.hpp"
+#include "Hash.hpp"
 #include "memlog.h" 
 #include "msgassert.h"
 
 struct ConfigStruct {
+  int hashTableSize;
   bool regmem;
   char inputFile[100], outputFile[100], logname[100] = "/tmp/email_server_log.out";
 };
@@ -65,6 +68,84 @@ void parse_args(int argc,char ** argv) {
     "Google Mail Server - output file name must be previously defined");  
 }
 
+void handleMailInsertion(std::ifstream &inputFile, Hash *globalHash) {
+  int userId = -1;
+  int emailId = -1;
+  int messageLength = -1;
+
+  inputFile >> userId;
+  inputFile >> emailId;
+  inputFile >> messageLength;
+
+  errorAssert(userId >= 0, "User id wasn't provided at mail insertion");
+  errorAssert(emailId >= 0, "Email id wasn't provided at mail insertion");
+  errorAssert(messageLength >= 0, "Invalid message length provided at mail insertion");
+  errorAssert(messageLength <= MAIL_MESSAGE_MAX_SIZE, "Number of words in the mail message exceeds the limit of 200");
+
+  std::string emailMessage;
+  std::string currentWord;
+
+  for (int i = 0; i < messageLength; i++) {
+    inputFile >> currentWord;
+    emailMessage.append(currentWord);
+    emailMessage.append(" ");
+
+    errorAssert(currentWord.size() <= MAIL_WORD_MAX_SIZE, "Mail word exceeds character limit");
+    errorAssert(currentWord.size() > MAIL_WORD_MIN_SIZE, "Mail word is smaller than minimum size");
+  }
+  emailMessage.pop_back();
+
+  Email *newMail = new Email(emailId, emailMessage);
+  globalHash->insert(userId, newMail);
+}
+
+void handleMailSearch(std::ifstream &inputFile, Hash *globalHash) {
+  int userId = -1;
+  int emailId = -1;
+
+  inputFile >> userId;
+  inputFile >> emailId;
+
+  errorAssert(userId >= 0, "User id wasn't provided at mail search");
+  errorAssert(emailId >= 0, "Email id wasn't provided at mail search");
+
+  Email *searchedMail = globalHash->searchEmail(userId, emailId);
+
+  std::cout << SEARCH_MAIL_TAG << " " << userId << " " << emailId << ": ";
+
+  if(searchedMail != NULL)
+    std::cout << searchedMail->getEmailMessage();
+  else 
+    std::cout << SEARCHED_MAIL_NOT_FOUND;
+
+  std::cout << std::endl;
+}
+
+void handleMailDelete(std::ifstream &inputFile) {
+
+}
+
+void inputFileReader(char inputFileName[], Hash *globalHash) {
+  std::ifstream inputFile(inputFileName);
+  errorAssert(inputFile.is_open(), "\nFailed to open input file");
+
+  inputFile >> config.hashTableSize;
+  globalHash = new Hash(config.hashTableSize);
+
+  std::string buffer;
+
+  while (!inputFile.eof()) {
+    inputFile >> buffer;
+    
+    if(buffer == INSERT_MAIL_TAG)
+      handleMailInsertion(inputFile, globalHash);
+    else if(buffer == SEARCH_MAIL_TAG)
+      handleMailSearch(inputFile, globalHash);
+    else if(buffer == DELETE_MAIL_TAG)
+      handleMailDelete(inputFile);
+  }
+}
+
 /**
  * @brief Main Function: Responsible for initializing the MemLog lib depending on the run-time 
  * parameters and calling the functions inputReadVectors followed by 
@@ -82,6 +163,13 @@ int main(int argc, char ** argv) {
     activateMemLog();
   else 
     deactivateMemLog();
+
+  Hash *globalHash = NULL;
+
+  inputFileReader(config.inputFile, globalHash);
+
+  // globalTree->print();
+  // std::cout << std::endl;
 
   return endUpMemLog();
 }
