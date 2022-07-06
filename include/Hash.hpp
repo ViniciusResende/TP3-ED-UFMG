6e0f0be79175b9865df8c7cd9c8866531824cdc8
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-07-06 11:57:35 -0300

    feat: impleenting inital hash table that uses binary tree as colision handler

---
+++ include/Hash.hpp
@@ -0,0 +1,23 @@
+#ifndef NODEH
+#define NODEH
+
+#include "Email.hpp"
+#include "msgassert.h"
+#include "memlog.h"
+#include "Tree.hpp"
+
+class Hash {
+  public:  
+    Hash(int hashTableSize);
+    void insert(int userId, Email* email);
+    void pop(int userId, int emailId);
+    Email* searchEmail(int userId, int emailId);
+    ~Hash();
+
+  private:
+    int hashTableSize;
+    int getHashIdx(int key);
+    Tree *Table;
+};
+
+#endif
---
+++ src/Hash.cpp
@@ -0,0 +1,23 @@
+#include "Hash.hpp"
+
+Hash::Hash(int hashTableSize) {
+  errorAssert(hashTableSize > 0, "Null Hash Table dimension");
+  this->hashTableSize = hashTableSize;
+
+  this->Table = (Tree*) malloc(this->hashTableSize * sizeof(Tree));
+  errorAssert(Table != NULL, "Failed to allocate Hash Table memory");
+}
+
+void Hash::insert(int userId, Email* email) {
+  int hashTableInsertionIdx = this->getHashIdx(userId);
+  this->Table[hashTableInsertionIdx].insert(email);
+}
+
+Email* Hash::searchEmail(int userId, int emailId) {
+  int hashTableSearchIdx = this->getHashIdx(userId);
+  return this->Table[hashTableSearchIdx].search(emailId);
+}
+
+int Hash::getHashIdx(int key) {
+  return key % this->hashTableSize;
+}
