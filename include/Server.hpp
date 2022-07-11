1be414b6ba3c1a3fc51d527c23397255f0385899
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-07-11 16:21:20 -0300

    feat: dodcumenting Server class

--- include/Server.hpp
+++ include/Server.hpp
@@ -9,14 +9,50 @@
 
 class Server {
   public:  
+    /**
+     * @brief Default constructor for Server class instance. Responsible for initialize
+     * the Server data structures and attributes.
+     * 
+     * @param inputFileName Name of the Server input file.
+     * @param outputFileName Name of the Server output file.
+     */
     Server(char inputFileName[], char outputFileName[]);
+
+    /**
+     * @brief Function responsible for executing all Server responsibilities from 
+     * reading the input file to print the operations results in the output file.
+     */
     void run();
+
+    /**
+     * @brief Default destructor of the class, that will clean it up when called and free the allocated memory.
+     */
     ~Server();
 
   private:
+    /**
+     * @brief Orchestrator function responsible for reading the server input file 
+     * and call the correct methods according with what is passed by the input 
+     * file.
+     */
     void inputFileReader();
+    /**
+     * @brief Function responsible for executing the insertion of an email in the 
+     * users Hash table and, by the end, print the operation metrics in the 
+     * output file.
+     */
     void handleMailInsertion();
+    /**
+     * @brief Function responsible for executing the search operation of an email 
+     * in the users Hash table and, by the end, print the operation metrics in 
+     * the output file.
+     */
     void handleMailSearch();
+    /**
+     * @brief Function responsible for executing the delete operation of an email 
+     * in the users Hash table and, by the end, print the operation metrics in 
+     * the output file.
+     */
     void handleMailDelete();
 
     Hash* userTable;
