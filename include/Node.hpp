21b98813d2c326467ec6e9deddec59e421a1212f
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-07-11 15:09:56 -0300

    feat: documenting Node class methods

--- include/Node.hpp
+++ include/Node.hpp
@@ -7,8 +7,17 @@
 
 class Node {
   public:  
+    /**
+     * @brief Default constructor for Node class instance. Responsible for initialize
+     * the Node attributes.
+     * 
+     * * @param email The email to be stored in the  node.
+     */
     Node(Email* email);
 
+    /**
+     * @brief Default destructor of the class, that will clean it up when called and free the allocated memory.
+     */
     ~Node();
 
   private:
